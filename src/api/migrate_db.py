a1c4484b4033b777df9023815d4990a157efeb50
behike56 <engineering@lambda-publishing.dev>
2024-12-31 14:49:23 +0900

    ADD(dev): テーブル作成用モジュール

---
+++ src/api/migrate_db.py
@@ -0,0 +1,21 @@
+import os
+from sqlalchemy import create_engine
+
+from src.api.models.task import Base
+
+DB_USER = os.environ.get("DB_USER", "dev")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "dev")
+DB_HOST = os.environ.get("DB_HOST", "db")
+DB_NAME = os.environ.get("DB_NAME", "devdb")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+print(DATABASE_URL)
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def migrate_db():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    migrate_db()
